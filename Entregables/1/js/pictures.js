b1ae610efea0af84b59415ca4e81e766be33546b
NinaEncinosa <67855315+NinaEncinosa@users.noreply.github.com>
2020-09-08 19:34:43 -0300

    Funcionalidad de cargar imagen y adaptarla

--- Entregables/1/js/pictures.js
+++ Entregables/1/js/pictures.js
@@ -3,9 +3,10 @@ window.addEventListener("load", () => {
     let canvas = document.querySelector("#canvas");
     let ctx = canvas.getContext("2d");
     let input = document.querySelector('.input1');
-    let filtro1 = document.querySelector('#sepia');
 
-    filtro1.addEventListener("click", sepia);
+    //   let filtro1 = document.querySelector('#sepia');
+
+    //   filtro1.addEventListener("click", sepia);
 
     input.onchange = e => {
 
@@ -26,12 +27,14 @@ window.addEventListener("load", () => {
             image.src = content;
 
             image.onload = function () {
-                let imageAspectRatio = (1.0 * this.height) / this.width;
-                let imageScaledWidth = canvas.width;
-                let imageScaledHeight = canvas.width * imageAspectRatio;
+                let scale = Math.min(canvas.width / this.width, canvas.height / this.height);
+
+                //               let imageAspectRatio = (1.0 * this.height) / this.width;
+                // let imageScaledWidth = canvas.width;
+                // let imageScaledHeight = canvas.width * imageAspectRatio;
 
                 // draw image on canvas
-                ctx.drawImage(this, 0, 0, imageScaledWidth, imageScaledHeight);
+                ctx.drawImage(this, 0, 0, this.width * scale, this.height * scale);
 
             }
         }
